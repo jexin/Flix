a5786d19a97a20e1612c55fba14c0cd14b34e8c6
Jenny Xin <jennyxin28@gmail.com>
2020-01-30 17:10:12 -0800

    add part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jenny Xin on 1/26/20.
+//  Copyright © 2020 Jenny Xin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
